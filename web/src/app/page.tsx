268846ac1a5d2e4d1cc9ca0f46470bc413b7d977
dev-gomes <juniorgomescrg@outlook.com>
2023-05-16 15:07:33 -0300

    Setup the project

---
+++ web/src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/Button";
+
+export default function Home() {
+  return (
+    <div className="h-screen bg-zinc-900">
+      <h1 className="text-zinc-50">Teste</h1>
+    </div>
+  )
+}
